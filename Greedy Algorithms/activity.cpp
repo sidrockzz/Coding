f36bcf0ccaf4e79349caf4789fa2917537939d6b
Siddharth <marshtomp.1999@hotmail.com>
2020-06-11 13:33:03 +0530

    Activity Problem in STL

--- Greedy Algorithms/activity.cpp
+++ Greedy Algorithms/activity.cpp
@@ -1,36 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Activitiy
-{
-    int start, finish;
-};
-bool activityCompare(Activitiy s1, Activitiy s2)
-{
-    return (s1.finish < s2.finish);
-}
-void printMaxActivities(Activitiy arr[], int n)
-{
-    sort(arr, arr+n, activityCompare);
-    cout << "Following activities are selected n";
-    int i = 0;
-    cout << "(" << arr[i].start << ", " << arr[i].finish << "), ";
-    for (int j = 1; j < n; j++)
-    {
-        if (arr[j].start >= arr[i].finish)
-        {
-            cout << "(" << arr[j].start << ", "
-                 << arr[j].finish << "), ";
-            i = j;
+void SelectActivities(vector<int>s,vector<int>f){
+    vector<pair<int,int>>ans;
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>p;
+
+    for(int i=0;i<s.size();i++){
+        p.push(make_pair(f[i],s[i]));
+    }
+
+    auto it = p.top();
+    int start = it.second;
+    int end = it.first;
+    p.pop();
+    ans.push_back(make_pair(start,end));
+
+    while(!p.empty()){
+        auto itr = p.top();
+        p.pop();
+        if(itr.second >= end){
+            start = itr.second;
+            end = itr.first;
+            ans.push_back(make_pair(start,end));
         }
+    }
+    cout << "Following Activities should be selected. " << endl << endl;
+    for(auto itr=ans.begin();itr!=ans.end();itr++){
+        cout << "Activity started at: " << (*itr).first << " and ends at  " << (*itr).second << endl;
     }
 }
 
 int main()
 {
-    Activitiy arr[] = {{5, 9}, {1, 2}, {3, 4}, {0, 6},
-                       {5, 7}, {8, 9}};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    printMaxActivities(arr, n);
+    vector<int>s = {1, 3, 0, 5, 8, 5};
+    vector<int>f = {2, 4, 6, 7, 9, 9};
+    SelectActivities(s,f);
     return 0;
 }
