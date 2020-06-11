566511e27ceb7ff55fde1856e032b713a67d8ac2
Siddharth <marshtomp.1999@hotmail.com>
2020-06-11 13:20:06 +0530

    Activity modified

--- Greedy Algorithms/activity.cpp
+++ Greedy Algorithms/activity.cpp
@@ -1,20 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
 
-
-#include<stdio.h>
-#include<iostream>
-void printMaxActivities(int s[], int f[], int n)
+struct Activitiy
 {
-    int i, j;
-    printf ("Following activities are selected n");
-    i = 0;
-    printf("%d ", i);
-
-
-    for (j = 1; j < n; j++)
+    int start, finish;
+};
+bool activityCompare(Activitiy s1, Activitiy s2)
+{
+    return (s1.finish < s2.finish);
+}
+void printMaxActivities(Activitiy arr[], int n)
+{
+    sort(arr, arr+n, activityCompare);
+    cout << "Following activities are selected n";
+    int i = 0;
+    cout << "(" << arr[i].start << ", " << arr[i].finish << "), ";
+    for (int j = 1; j < n; j++)
     {
-        if (s[j] >= f[i])
+        if (arr[j].start >= arr[i].finish)
         {
-            printf ("%d ", j);
+            cout << "(" << arr[j].start << ", "
+                 << arr[j].finish << "), ";
             i = j;
         }
     }
@@ -22,9 +28,9 @@ void printMaxActivities(int s[], int f[], int n)
 
 int main()
 {
-    int s[] =  {1, 3, 0, 5, 8, 5};
-    int f[] =  {2, 4, 6, 7, 9, 9};
-    int n = sizeof(s)/sizeof(s[0]);
-    printMaxActivities(s, f, n);
+    Activitiy arr[] = {{5, 9}, {1, 2}, {3, 4}, {0, 6},
+                       {5, 7}, {8, 9}};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printMaxActivities(arr, n);
     return 0;
-}
+} 
