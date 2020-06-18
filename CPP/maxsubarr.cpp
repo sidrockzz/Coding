2eacc342471e5a7b1d784a7d42fd92ce7f0379aa
Siddharth <marshtomp.1999@hotmail.com>
2020-06-18 14:27:18 +0530

    Second algorithm for maximum subarray

--- CPP/maxsubarr.cpp
+++ CPP/maxsubarr.cpp
@@ -16,14 +16,12 @@ int main(){
     }
     int best = 0;
     for (int a = 0; a < n; a++) {
-        for (int b = a; b < n; b++) {
             int sum = 0;
-            for (int k = a; k <= b; k++) {
-                sum += array[k];
+            for (int b = a; b < n; b++) {
+                sum += array[b];
+                best = max(best,sum);
             }
-            best = max(best,sum);
         }
-    }
     cout << best << "\n";
 }
-//O(n^3) algo
+//O(n^2) algo
