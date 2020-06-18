66bab054c58aa541e3c82f1990ea2ff602f8dead
Siddharth <marshtomp.1999@hotmail.com>
2020-06-18 11:15:50 +0530

    Shortening the code using define and macros

--- CPP/factorial.cpp
+++ CPP/factorial.cpp
@@ -3,17 +3,30 @@
 //
 
 #include<bits/stdc++.h>
+#include <vector>
 #define M 1000000007
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define SQ(a) (a)*(a)
 using namespace std;
+typedef long long ll;
+typedef vector<int>vi;
+typedef pair<int,int>pi;
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     //freopen("input.txt","x","stdin");
    // freopen('output.txt',"w","stdout");
-    long long n,x=1;
+    unsigned long long n,x=1;
     cin>>n;
-    for(int i =1;i<=n;i++)
+    n = n%M;
+    if(n<0)
+        n+=M;
+    REP(i,1,n)
         x = (x*i)%M;
     cout<<x%M<<"\n";
    /// fclose(stdin);
