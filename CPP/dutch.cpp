5e93804ba3cdfe503956dd054191f4ae4363fd84
Siddharth <marshtomp.1999@hotmail.com>
2020-06-18 10:52:40 +0530

    Unknown number of data to be added

---
+++ CPP/dutch.cpp
@@ -0,0 +1,18 @@
+//
+// Created by siddharth on 18/06/20.
+//
+//Dutch National Problem
+//IF the data is unknown
+#include<iostream>
+#include<string>
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+int main()
+{
+    string s;
+    while (cin >> s)
+        cout << s << endl;
+    return 0;
+}
