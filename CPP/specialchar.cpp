2bc79a3682959badc8fdd0741e01218e8c986ed9
Siddharth <marshtomp.1999@hotmail.com>
2020-07-02 11:14:03 +0530

    Special Character Count

--- CPP/specialchar.cpp
+++ CPP/specialchar.cpp
@@ -3,3 +3,47 @@
 //
 
 // Solve thw problem based on number of special characters. If the count is odd print first odd digits and next even digits (vice versa)
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string s;
+    int i,c=0;
+    cin>>s;
+    //cout<<s;
+    for(int i=0;i<s.length();i++){
+        //prints the alphabets and number
+        if(!((s[i]>='A' && s[i] <= 'Z')||(s[i]>='a' && s[i] <= 'z')||(s[i]>=48 && s[i]<=57)))
+            c++;
+    }
+    if(c%2==0){
+        for(i=0;i<s.length();i++){
+            if(s[i]>=48 && s[i]<=57){
+                if(s[i]%2==0)
+                    cout<<s[i];
+            }
+        }
+        for(i=0;i<s.length();i++) {
+            if (s[i] >= 48 && s[i] <= 57) {
+                if (s[i] % 2 != 0)
+                    cout << s[i];
+            }
+        }
+    }
+    else{
+        for(i=0;i<s.length();i++) {
+            if (s[i] >= 48 && s[i] <= 57) {
+                if (s[i] % 2 != 0)
+                    cout << s[i];
+            }
+        }
+        for(i=0;i<s.length();i++){
+            if(s[i]>=48 && s[i]<=57){
+                if(s[i]%2==0)
+                    cout<<s[i];
+            }
+        }
+    }
+}
