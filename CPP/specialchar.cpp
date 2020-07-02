0e296d4ff336e4133acc55ade1cf242c4dbf3064
Siddharth <marshtomp.1999@hotmail.com>
2020-07-02 10:59:05 +0530

    Kth element optimised

---
+++ CPP/specialchar.cpp
@@ -0,0 +1,5 @@
+//
+// Created by siddharth on 02/07/20.
+//
+
+// Solve thw problem based on number of special characters. If the count is odd print first odd digits and next even digits (vice versa)
