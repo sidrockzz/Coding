a6306c2e0b988e4800010be502c76bfd45fd467b
Siddharth <marshtomp.1999@hotmail.com>
2020-07-02 10:53:40 +0530

    Factors kth element

--- CPP/kth.cpp
+++ CPP/kth.cpp
@@ -5,25 +5,20 @@
 using namespace std;
 // Given number is n and k find the kth largest factor
 int main(){
-    int n,k,i,a=0,arr[50];
+    int n,k,i,c=0;
     cin>>n;
     cin>>k;
     //finds all the factors
-    for(int i=1;i<=n;i++){
-        if(n%i==0){
-            arr[a]=i;
-            a++;
+    for(int i=n;i>=1;i--){
+        if(n%i==0) {
+            c++;
+        }
+        if(c==k){
+            cout<<i<<"\n";
+            break;
         }
     }
-  /*  for(i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }*/
-  //If the factors is less than given k return 1
-  if(a<k){
-      cout<<"1\n";
-  }
-  else{
-      cout<<arr[a-k]<<"\n";
-  }
+    if(c<k)
+        cout<<"1\n";
 return 0;
 }
