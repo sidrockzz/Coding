ce411baf5ef818481aa6d69ea29f0fc5c8ef633f
Siddharth Datthathreya <marshtomp.1999@hotmail.com>
2020-12-07 15:09:03 +0530

    GCD and Common divisor with gcd

--- Python/common_dvisor.py
+++ Python/common_dvisor.py
@@ -1,6 +1,12 @@
+#!/usr/bin/python
+def gcd(a,b):
+    if(b==0):
+        return a
+    else:
+        return gcd(b,a%b)
 def cf(num1,num2):
     n=[]
-    for i in range(1, min(num1,num2)+1): 
+    for i in range(1, gcd(num1,num2)+1): 
         if num1%i==num2%i==0: 
             n.append(i)
     return n
---
+++ Python/gcd.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+def gcd(a,b):
+    if(b==0):
+        return a
+    else:
+        return gcd(b,a%b)
+
+n1 = int(input())
+n2 = int(input())
+
+print(gcd(n1,n2))
