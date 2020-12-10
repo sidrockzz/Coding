b2e392461ee341879b5a806fd5c87a853c7220bf
Siddharth Datthathreya <marshtomp.1999@hotmail.com>
2020-12-10 15:22:01 +0530

    Matrix Diagonal sub optimisied

--- Python/matrix_diag.py
+++ Python/matrix_diag.py
@@ -6,8 +6,18 @@ d1 = 0
 d2 = 0
 n = len(mat)
 print(n)
-for i in range(n): 
+"""for i in range(n): 
     d1 += mat[i][i] 
     d2 += mat[i][n - i - 1] 
+"""
+count = 0
+j1 =0
+j2=n-1
 
+while(count<n):
+    d1 +=mat[count][j1]
+    d2 +=mat[count][j2]
+    count+=1
+    j1+=1
+    j2-=1
 print(str(abs(d2-d1)))
