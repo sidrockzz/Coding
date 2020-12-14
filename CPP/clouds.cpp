e9bb45bf5a636166c6d4b3f37a87bb37917c5814
Siddharth Datthathreya <marshtomp.1999@hotmail.com>
2020-12-14 15:09:49 +0530

    Warmup Cloud

---
+++ CPP/clouds.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the jumpingOnClouds function below.
+int jumpingOnClouds(vector<int> clouds) {
+    int jumps = 0;
+    int n = clouds.size();
+    for (int i = 0; i < n; i++) {
+        if (i+2 < n && !clouds[i+2]) {
+            jumps++;
+            i++;
+        } else if (i+1 < n && !clouds[i+1]) {
+            jumps++;
+        }
+    }
+    return jumps;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string c_temp_temp;
+    getline(cin, c_temp_temp);
+
+    vector<string> c_temp = split_string(c_temp_temp);
+
+    vector<int> c(n);
+
+    for (int i = 0; i < n; i++) {
+        int c_item = stoi(c_temp[i]);
+
+        c[i] = c_item;
+    }
+
+    int result = jumpingOnClouds(c);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
